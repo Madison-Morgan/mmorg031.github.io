92b76d3be2d54d272ab09094532207bb03a0bc7d
Madison Morgan <mmorg031@uottawa.ca>
2020-10-25 17:43:52 -0400

    fixing carousel to work properly and look good

--- js/scripts/carousel_init.js
+++ js/scripts/carousel_init.js
@@ -1,7 +1,4 @@
 $(function(){
-    /* <div class="carousel-item active">
-        <img class="d-block w-100" src="images/trips/venice.heic" alt="venice.heic">
-    </div> */
     var firstActive = true;
 
     $.ajax({
@@ -9,19 +6,25 @@ $(function(){
         success: function (data) {
             $(data).find("a:contains(.jpg)").each(function () {
                 var divItem = $("<div></div>").addClass("carousel-item");  
+                var divHeader = $("<div></div>").addClass("carousel-caption d-none d-md-block");
+
                 if(firstActive){
                     divItem.addClass("active");
                     firstActive = false;
                 }
-                // will loop through 
                 var images = $(this).attr("href");
                 images = images.substring(1);
 
                 var imgItem = $("<img />").addClass("d-block w-100");
                 imgItem.attr("src",images);
                 imgItem.attr("alt",images);
+                
+                var headerTag = $("<h5></h5>").text(images.replace(/_/g," ").replace("images/trips/","").replace(".jpg",""));
+                headerTag.addClass("photo-carousel-header");
+                divHeader.append(headerTag);
 
                 divItem.append(imgItem);
+                divItem.append(divHeader);
                 divItem.appendTo('#carousel-inner-photos');
 
             });
