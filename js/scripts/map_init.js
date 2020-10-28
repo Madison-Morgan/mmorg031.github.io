835e1577d1712a04ad8de06f87f29d343dcc8e09
Madison Morgan <mmorg031@uottawa.ca>
2020-10-28 16:37:24 -0400

    Adding map to website and flex

--- js/scripts/map_init.js
+++ js/scripts/map_init.js
@@ -1,7 +1,14 @@
-mapboxgl.accessToken = '<your access token here>';
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibW1vcmcwMzEiLCJhIjoiY2tndGxmNnhnMDRtaTJ5cWY0ZGRqOHJ1YyJ9.AjixPunpSrsx_53OQxyE0w';
+
 var map = new mapboxgl.Map({
 container: 'map', // container id
-style: 'mapbox://styles/mapbox/streets-v11', // style URL
+style: 'mapbox://styles/mapbox/dark-v10', // style URL
 center: [-74.5, 40], // starting position [lng, lat]
-zoom: 9 // starting zoom
+zoom: 6 // starting zoom
 });
+
+var marker = new mapboxgl.Marker()
+.setLngLat([12.550343,55.66])
+.addTo(map);
